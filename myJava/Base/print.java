9e36a75fa73a94b4ccea87a7885d3748438ed9c1
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-10-28 19:30:14 +0530

    Create print.java

---
+++ myJava/Base/print.java
@@ -0,0 +1,37 @@
+public class print {
+
+    //print promot type => int
+    public static void i(int i_ids) {
+        System.out.print(i_ids);
+    }
+
+    //print promot type => string
+    public static void s(String s_names) {
+        System.out.print(s_names);
+    }
+
+    //print promot type => float
+    public static void f(float f_float) {
+        System.out.print(f_float);
+    }
+
+    //print promot type => double
+    public static void d(double d_double) {
+        System.out.print(d_double);
+    }
+
+    //print promot type => long
+    public static void l(long l_long) {
+        System.out.print(l_long);
+    }
+
+    //print promot type => char
+    public static void c(char c_char) {
+        System.out.print(c_char);
+    }
+
+    //print promot type => boolean
+    public static void b(boolean b_boolean) {
+        System.out.print(b_boolean);
+    }
+}
