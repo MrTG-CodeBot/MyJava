4fa8bae99145bf5be8f6bec3b714ff479a8c9242
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-10-31 23:57:40 +0530

    Update sample.java

--- myJava/math/sample.java
+++ myJava/math/sample.java
@@ -19,19 +19,47 @@ public class sample {
         boolean iseven = math.isEven(4);
         boolean isodd = math.isOdd(5);
 
+        boolean ipass = math.isPass(60, 28);
+        boolean fpass = math.isPass(60f, 28f);
+        boolean dpass = math.isPass(60.0, 28.0);
+
+        boolean ifail = math.isFail(24, 28);
+        boolean ffail = math.isFail(60f, 28f);
+        boolean dfail = math.isFail(60.0, 28.0);
+
         print.s("Sum: " + sum + "\n");
         print.s("Float Sum: " + fsum + "\n");
         print.s("Double Sum: " + dsum + "\n");
+        
         print.s("Difference: " + difference + "\n");
         print.s("Float Difference: " + fdifference + "\n");
         print.s("Double Difference: " + ddifference + "\n");
+        
         print.s("Multiplication: " + multiplication + "\n");
         print.s("Double Multiplication: " + dmultiplication + "\n");
         print.s("Float Multiplication: " + fmultiplication + "\n");
+        
         print.s("Division: " + division + "\n");
         print.s("Float Division: " + fdivision + "\n");
         print.s("Division: " + ddivision + "\n");
+        
         print.s("Is Even: " + iseven + "\n");
         print.s("Is Odd: " + isodd + "\n");
+        
+
+        if (ipass) {
+            print.s("You are passed, congrats\n");
+        }
+        else{
+            print.s("You are not passed, " + math.sub(60, 5) + " marks for to win the exam.\n");
+        }
+        
+
+        if (ifail) {
+            print.s("You are not passed, " + math.sub(28, 24) + " marks for to win the exam.\n");
+        }
+        else{
+            print.s("You are passed, congrats\n");
+        }
     }
 }
